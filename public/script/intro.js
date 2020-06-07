d7b1506b682a6ce24d30d8ca8cc21294aa02290a
Priscilla P <passaropriscilla@gmail.com>
2020-06-07 05:18:59 -0300

    primeiro_commit

---
+++ public/script/intro.js
@@ -0,0 +1,66 @@
+//coments
+// document.write("Hello");
+//variaveis, tipos de dados
+// let myvar = "HE";
+// const myconst = "HE";
+// document.write(myconst + myvar);
+
+//string 
+// const s1 = "Isto é uma string"
+// const s2 = 'Isso também é uma string'
+// const s3 = `Isso é uma string também`
+
+//number
+// const n1 = 1;
+// const n2 = 12;
+// document.write(n1 + n2);
+
+//boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bTrue)
+
+//objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar uma grande logica de correr")
+//     return "Run Forest"
+//   }
+// }
+// pessoa.correr()
+// document.write(pessoa.correr())
+
+//Array - Vetores - Coleção de dados
+// const colecaodebolinhas = ["blue", "green", 1, {name: "My Name"}]
+
+// document.write(colecaodebolinhas[3].name)
+
+//funções - funcionalidades - atalhos - reuso de código
+
+//registrar função 
+// function saymyname(name){
+//   document.write(name)
+// }
+// executar
+// saymyname("Shirley")
+// saymyname("Carol")
+
+//condicionais
+
+// const notafinal = 4
+
+// if (notafinal < 5 ){
+//   document.write("Reprovado")
+// } else {
+//   document.write("Aprovado")
+// }
+
+
+//loop repetições
+// for (i = 0; i < 10; i++){
+//   document.write(`<p>${i}</p>`)
+// }
+
